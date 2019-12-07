16671556966e3998dac70d005ec1019416aba61d
Albert Hunor-Arnold <hunorarnold@gmail.com>
2019-12-07 22:48:22 +0200

    Date & Time picker

--- AdminApplication/app/src/main/java/com/example/adminapplication/MainActivity.java
+++ AdminApplication/app/src/main/java/com/example/adminapplication/MainActivity.java
@@ -3,13 +3,83 @@ package com.example.adminapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.sql.Time;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDate, btnTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDate = findViewById(R.id.btn_pickDate);
+        btnTime = findViewById(R.id.btn_pickTime);
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleDateButton();
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleTimeButton();
+            }
+        });
+    }
+
+    private void handleTimeButton() {
+
+        Calendar calendar = Calendar.getInstance();
+        int HOUR = calendar.get(Calendar.HOUR);
+        int MINUTE = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+
+                String timeString = hour + ":" + minute;
+                btnTime.setText(timeString);
+
+            }
+        }, HOUR, MINUTE, true);
+
+        timePickerDialog.show();
+
+    }
+
+    private void handleDateButton() {
+
+        Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+
+                String dateString = year + "." + month + "." + date;
+                btnDate.setText(dateString);
+
+            }
+        }, YEAR, MONTH, DATE);
+
+        datePickerDialog.show();
+
     }
 }
